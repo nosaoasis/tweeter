9ce13f695f66ee48f31ee5e330e94829d2b2c332
Nosa Agho <nosaoasis@gmail.com>
2023-05-04 20:11:45 +0100

    Dynamic Tweets
    Implemented the dynamic rendering of the tweets. Created a 'createTweetElement' and 'renderTweet' functions to create the tweet DOM structure dynamically and prepend the tweets to the 'tweets-container'.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,71 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+// render tweets
+const renderTweets = tweets => {
+  tweets.forEach(tweet => {
+    let $tweet = createTweetElement(tweet);
+    $("#tweets-container").prepend($tweet);
+  });
+};
+
+// create a new tweet component
+const createTweetElement = tweetData => {
+  const { user, content, created_at } = tweetData;
+
+  let singleTweetElement = $(`<article class="tweet-component">
+        <!-- image-username-refkey -->
+        <div class="image-username-refkey">
+          <div class="image-username">
+            <img src=${user.avatars} alt="" />
+            <span>${user.name}</span>
+          </div>
+          <div>${user.handle}</div>
+        </div>
+        <!-- tweet contect -->
+        <div class="tweet-content">
+          <p>${content.text}</p>
+        </div>
+        <!-- time and reactions icons -->
+        <div class="time-reactions">
+          <p>${created_at}</p>
+          <div class="icons">
+            <i class="fas fa-flag"></i>
+            <i class="fas fa-retweet"></i>
+            <i class="fas fa-heart"></i>
+          </div>
+        </div>
+      </article>`);
+
+  return singleTweetElement;
+};
+
+
+$(document).ready(function() {
+  renderTweets(data)
+})
