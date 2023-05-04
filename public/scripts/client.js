a9a4cfc0a8cbb97a15b350dd431fdfaa19d0e4f6
Nosa Agho <nosaoasis@gmail.com>
2023-05-04 20:26:02 +0100

    Fetching Tweets with Ajax
    Implemented the 'loadTweets' function which makes a api call to fetch the tweets from the server and send the data to the renderTweets function.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,39 +8,39 @@ let endpoint = "/tweets";
 
 const data = [
   {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
+    content: {
+      text: "If I have seen further it is by standing on the shoulders of giants",
     },
-    "created_at": 1461116232227
+    created_at: 1461116232227,
   },
   {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd",
     },
-    "created_at": 1461113959088
-  }
-]
+    content: {
+      text: "Je pense , donc je suis",
+    },
+    created_at: 1461113959088,
+  },
+];
 
 // render tweets
-const renderTweets = tweets => {
-  tweets.forEach(tweet => {
+const renderTweets = (tweets) => {
+  tweets.forEach((tweet) => {
     let $tweet = createTweetElement(tweet);
     $("#tweets-container").prepend($tweet);
   });
 };
 
 // create a new tweet component
-const createTweetElement = tweetData => {
+const createTweetElement = (tweetData) => {
   const { user, content, created_at } = tweetData;
 
   let singleTweetElement = $(`<article class="tweet-component">
@@ -54,11 +54,11 @@ const createTweetElement = tweetData => {
         </div>
         <!-- tweet contect -->
         <div class="tweet-content">
-          <p>${content.text}</p>
+        <p>${content.text}</p>
         </div>
         <!-- time and reactions icons -->
         <div class="time-reactions">
-          <p>${created_at}</p>
+        <p>${timeago.format(created_at)}</p>
           <div class="icons">
             <i class="fas fa-flag"></i>
             <i class="fas fa-retweet"></i>
@@ -70,25 +70,37 @@ const createTweetElement = tweetData => {
   return singleTweetElement;
 };
 
+// loading the tweets
+const loadTweets = () => {
+  $.ajax({
+    method: "GET",
+    url: endpoint,
+    dataType: "json",
+    success: function (data) {
+      renderTweets(data);
+    },
+  });
+};
 
-$(document).ready(function() {
-  renderTweets(data)
+$(document).ready(function () {
 
-  $("form").on("submit", function(e) {
+  $("form").on("submit", function (e) {
     e.preventDefault();
-    
+
     $.ajax({
       method: "POST",
       url: endpoint,
       type: "application/json",
       data: $(this).serialize(),
-      success: function() {
+      success: function () {
         $("textarea").val("");
-        $.get("http://localhost:8080/tweets", data => {
+        $.get("http://localhost:8080/tweets", (data) => {
           const newTweet = [data.slice(-1).pop()];
           renderTweets(newTweet);
         });
-      }
+      },
     });
   });
-})
+
+  loadTweets();
+});
